7dc54bdf611e4e8fcaf5a8c69c610d7964d3eb4d
AyushKDutta271 <unbreakableme271@gmail.com>
2026-04-14 23:29:24 +0530

    Addeed more Javascript like DOM manipulation, CSS manipulation and async function

--- JS-Files/index.js
+++ JS-Files/index.js
@@ -13,30 +13,60 @@ function getComputerChoice()
     }
 }
 
-function getHumanChoice()
-{
-    let round =0;
-    while(round<=5)
-    {
-    let computerChoice = getComputerChoice();
-    // console.log("computer choice is :"+computerChoice); 
-    let humanChoice = prompt("Rock, Paper Or Scissor").toLowerCase();
-    // console.log("human choice is :"+humanChoice); 
+let rock=document.getElementById('rock');
+let paper=document.getElementById('paper');
+let scissor=document.getElementById('scissor');
 
-    if(playRound(computerChoice, humanChoice)=="tie")
+
+rock.addEventListener('click',()=>getHumanChoice("rock"));
+paper.addEventListener('click',()=>getHumanChoice("paper"));
+scissor.addEventListener('click',()=>getHumanChoice("scissor"));
+
+let round =0;
+function getHumanChoice(choice)
+{
+
+    
+    
+    let computerChoice=getComputerChoice();
+    console.log("computer choice is :"+computerChoice); 
+    
+    console.log("human choice is :"+choice); 
+    document.querySelector(".results").innerHTML=`<h3>Current Round: ${round}</h3>`;
+    if(playRound(computerChoice,choice)=="tie")
     {
         round=0;
     }
+    else{
     round++;
     }
-
+    
+   if(round==5)
+   {
     if(computerScore>humanScore)
     {
+        let res=document.querySelector(".results");
+        let conc=document.createElement("p");
+        conc.textContent="FINAL WINNER IS COMPUTER";
+        res.appendChild(conc);
+        let result=document.querySelector(".results");
+        let conc1=document.createElement("p");
+        conc1.innerText=`Final Scores: computer score= ${computerScore}; human score =${humanScore}`;
+        result.appendChild(conc1);
         console.log("Computer is the Final Winner!" + `Scores: Computer Score=${computerScore} , Your Score =${humanScore} `);
 
     }
     else if(humanScore>computerScore)
     {
+        let res=document.querySelector(".results");
+        let conc=document.createElement("p");
+        conc.textContent="FINAL WINNER IS HUMAN";
+        res.appendChild(conc);
+        let result=document.querySelector(".results");
+        let conc1=document.createElement("p");
+        conc1.innerText=`Final Scores: computer score= ${computerScore}; human score =${humanScore}`;
+        result.appendChild(conc1);
+
         console.log("Human is the Final Winner!");
     }
     else{
@@ -44,31 +74,56 @@ function getHumanChoice()
     }
 }
 
+}
+
 function playRound(computerChoice,humanChoice)
 {
 
-    // console.log("current round is: "+round);
+   
+   
    if((computerChoice=="rock" && humanChoice=="scissor") || (computerChoice=="scissor" && humanChoice=="paper") || (computerChoice=="paper" && humanChoice=="rock") )
    {
-    console.log("Computer Beats Human(You)");
+   let res= document.querySelector(".results");
+   let pTag=document.createElement("p");
+   pTag.innerText="Computer Beats Human (You)!";
+
+   res.appendChild(pTag);
     computerScore++;
    }
     
    else if((humanChoice==="rock" && computerChoice==="scissor") || (humanChoice==="scissor" && computerChoice==="paper") || (humanChoice==="paper" && computerChoice==="rock"))
    {
-     console.log("You Beats Computer!");
+     let res= document.querySelector(".results");
+   let pTag=document.createElement("p");
+   pTag.innerText="Human (You) beats Computer!";
+
+   res.appendChild(pTag);
      humanScore++;
    }
 
    else{
     console.log("Both are helpless to beat each other! \nLet's try again!");
-    humanScore=0;
-    computerScore=0;
+   
+     let res= document.querySelector(".results");
+   let pTag=document.createElement("p");
+   pTag.innerText="Both are helpless to beat each other! \nLet's try again!";
+
+   res.appendChild(pTag);
     return "tie";
    }
 
 }
 
-getHumanChoice();
+let ins=document.getElementById('instruct');
+ins.style.display="none";
 
 
+setTimeout(()=>
+{
+    ins.style.display="block";
+},6000);
+
+let close=document.querySelector(".close");
+close.addEventListener('click',()=>{
+    ins.style.display="none";
+})
